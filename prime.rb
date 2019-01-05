203fbb536260e7ce35b06f53463422e525820b11
David Gilbert <dagilbert333@gmail.com>
2019-01-05 14:00:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,13 +2,13 @@
 def prime?(num)
   if num >= 2
     for d in 2..(num - 1)
-     if (num % d) == 0
-      return false
-     end
+      if (num % d) == 0
+        return false
+      end
     end
-  true
- else
-   false
- end
- end
+    true
+  else
+    false
+  end
+end
 
